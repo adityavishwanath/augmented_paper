221c55b5a31afb7e56ff0724d1afcc4313e33c5b
Azra Ismail <aismail1997@gatech.edu>
2016-09-18 01:44:39 -0400

    aditya was sleeping

--- phase2/mypaper/mypaper.ino
+++ phase2/mypaper/mypaper.ino
@@ -33,8 +33,8 @@ void loop()
 {
   // establish variables for duration of the ping,
   // and the distance result in inches and centimeters:
-  long duration1, inches1, cm1;
-  long duration2, inches2, cm2;
+  float duration1, inches1, cm1;
+  float duration2, inches2, cm2;
 
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
@@ -71,36 +71,37 @@ void loop()
   cm2 = microsecondsToCentimeters(duration2);
 
   if (inches1 <= 12 && inches2 <= 12) {
-  Serial.print("x: ");
-  Serial.print(inches1);
-  Serial.print("in, ");
+  //Serial.print("x: ");
+  //Serial.print(inches1);
+  //Serial.print("in, ");
   Serial.print(cm1);
-  Serial.print("cm\t");
+  //Serial.print("cm\t");
 
-  Serial.print("y: ");
-  Serial.print(inches2);
-  Serial.print("in, ");
+  //Serial.print("y: ");
+  //Serial.print(inches2);
+  //Serial.print("in, ");
+  Serial.print(",");
   Serial.print(cm2);
-  Serial.print("cm");
+  //Serial.print("cm");
   Serial.println();
   }
   delay(50);
 }
 
-long microsecondsToInches(long microseconds)
+float microsecondsToInches(long microseconds)
 {
   // According to Parallax's datasheet for the PING))), there are
   // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
   // second).  This gives the distance travelled by the ping, outbound
   // and return, so we divide by 2 to get the distance of the obstacle.
   // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
+  return (float)microseconds / 74.0 / 2.0;
 }
 
-long microsecondsToCentimeters(long microseconds)
+float microsecondsToCentimeters(long microseconds)
 {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
   // The ping travels out and back, so to find the distance of the
   // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
+  return (float)microseconds / 29.0 / 2.0;
 }
--- phase2/myreceiver.py
+++ phase2/myreceiver.py
@@ -1,16 +1,48 @@
+#!/usr/bin/env python
+
 # This code reads the data being printed out to the serial
 # The data is the processed ***
 
 import serial #Import Serial Library
 
+# set the baudrate and serial port
 arduinoSerialData = serial.Serial('com4', 115200)
 
 # read in text file with hard coded values
+# create a map of text to the web
+f = open('new_map.txt', 'r')
+nextline = f.readline()
 
+# create a dictionary mapping text to coordinates
+my_coordinates = {}
+coordinate = [0, 0]
+
+while (nextline):
+    dummyvalues = nextline.split(",")
+    item = dummyvalues[0]
+    coordinate[0] = float(dummyvalues[1])
+    coordinate[1] = float(dummyvalues[2])
+    my_coordinates[item] = coordinate;
+    nextline = f.readline()
 
 # if the average of the last five values obtained from the arduino is within 1 cm of one of the coordinates on the file, do a google search on that word and return the first link
 
+last_five_x = [0,0,0,0,0]
+last_five_y = [0,0,0,0,0]
+
+# reading from serial
 while (1==1):
     if (arduinoSerialData.inWaiting()>0):
         myData = arduinoSerialData.readline()
         print myData
+        if (myData):
+            data = myData.split(",")
+            del last_five_x[0]
+            del last_five_y[0]
+            last_five_x.append(float(data[0]))
+            last_five_y.append(float(data[1]))
+            avg_x = sum(last_five_x) / 5
+            avg_y = sum(last_five_y) / 5
+            for key in my_coordinates:
+                if avg_x > (my_coordinates[key][0] - 2) and avg_x < (my_coordinates[key][0] + 2) and avg_y > (my_coordinates[key][1] - 2) and avg_y < (my_coordinates[key][1] + 2):
+                    print key
